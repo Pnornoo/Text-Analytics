33223d93385db38f6844d9c3d99c42efbbb9575f
Pnornoo <112109100+Pnornoo@users.noreply.github.com>
2023-05-09 22:25:44 -0400

    Update Nornoo-A1.R

--- Nornoo-A1.R
+++ Nornoo-A1.R
@@ -1,34 +1,31 @@
 #CIS 428 HW1 - R basics
 
-#2. Write a function to get help about package tidyverse.
+#1. Write a function to get help about package tidyverse.
 help(package = 'tidyverse')
 
-#3. Write a function to set a working directory 
-setwd("/Users/priscilla/Desktop/Spring 2023 Semester/CIS 428")
-
-#4. Create a vector of courses that you are taking for last fall semester using the c() function. Hint Course names
+#2. Create a vector of courses that you are taking for last fall semester using the c() function. Hint Course names
 FallCourses <- c("Advanced Data Management & Analysis", "Business Analytics", "Data Visualization and Dashboards",
                  "Ethical and Societal Concerns of Computer Scientists", 
                  "Introduction to Data Analytics", "System Architecture and Integration")
 is.vector(FallCourses)
 
-#4a. Write a function to show how many elements in the vector
+#3a. Write a function to show how many elements in the vector
 length(FallCourses)
 
-#4b. Write a function to overview of the structure
+#3b. Write a function to overview of the structure
 str(FallCourses)
 
-#4c. Extract one value from the vector
+#3c. Extract one value from the vector
 FallCourses[4]
 
-#4d. Extract two values from the vector
+#3d. Extract two values from the vector
 FallCourses[2:3]
 
-#5. Create another vector of scores that corresponds to the number of elements in the vector that you created on #4. 
+#4. Create another vector of scores that corresponds to the number of elements in the vector that you created on #4. 
 scores <- c(90,80,70,60,50,90)
 length(scores)
 
-#5a. Write functions to show the average, minimum and maximum 
+#5. Write functions to show the average, minimum and maximum 
 mean(scores)
 min(scores)
 max(scores)
